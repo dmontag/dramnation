8e8ef3a99ce6fa1fb5f6e804a05a3fe09e70e548
dmontag <david@montag.se>
2014-05-24 23:56:31 +0200

    Added static content to web.js

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var logfmt = require("logfmt");
 var app = express();
 
 app.use(logfmt.requestLogger());
+app.use(express.static("./"));
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+// app.get('/', function(req, res) {
+//   res.send('Hello World!');
+// });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
